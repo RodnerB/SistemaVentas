bdb890fb92262bee81230db04785d7f69b436c25
Rodner Batista <orodnerbis1@gmail.com>
2025-11-14 17:50:50 -0400

    Adding the sqlConnection NuGet package and creating a class for the connection

---
+++ SistemaVentas/ConexionDB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace SistemaVentas
+{
+    
+    internal class ConexionDB
+    {
+        private static string connectionString = @"Server=LAPTOP-TDE5MEQB;Database=BDPROGRAMACION3;Trusted_Connection=True;";
+
+        public static SqlConnection ObtenerConexion()
+        {
+            SqlConnection conexion = new(connectionString);
+            try
+            {
+                conexion.Open();
+                return conexion;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Error al conectar a la base de datos" + ex.Message);
+            }
+        }
+
+        public static bool ProbarConexion()
+        {
+            try
+            {
+                using (SqlConnection conexion = ObtenerConexion())
+                {
+                    return conexion.State == ConnectionState.Open;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
