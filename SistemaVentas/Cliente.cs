b1c3024d25669f7eb541c767a82a2627dee75f96
Rodner Batista <orodnerbis1@gmail.com>
2025-11-15 18:23:28 -0400

    Adding the Clientes Class to the proyect for a proper handling of the clients info

---
+++ SistemaVentas/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas
+{
+    internal class Cliente
+    {
+        public required string CodigoCliente { get; set; }
+        public string NombreCliente { get; set; }
+        public required string ApellidoCliente { get; set; }
+        public required string DireccionCliente { get; set; }
+        public required string SectorCliente { get; set; }
+        public required string CiudadCliente { get; set; }
+        public required string TelefonoCliente { get; set; }
+        public string FaxCliente { get; set; } = string.Empty;
+        public decimal LimiteCreditoCliente { get; set; } = 0;
+        public decimal BalanceActualCliente { get; set; } = 0;
+        public required string ObservacionesCliente { get; set; }
+
+    }
+}
