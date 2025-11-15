9890bf817799524a705667bbd24817e7335008bb
Jose-chess <josejimenez76@alumno.utesa.edu>
2025-11-15 14:34:07 -0400

    modifique la conexion a la bd para que utiilice un archivo cs para recuperar la conexion, asi evitar conflictos entre cada computadora

--- SistemaVentas/ConexionDB.cs
+++ SistemaVentas/ConexionDB.cs
@@ -7,7 +7,7 @@ namespace SistemaVentas
     
     internal class ConexionDB
     {
-        private static string connectionString = @"Server=LAPTOP-TDE5MEQB;Database=BDPROGRAMACION3;Trusted_Connection=True;";
+        private static string connectionString = ConnectionString.GetConnectionString();
 
         public static SqlConnection ObtenerConexion()
         {
